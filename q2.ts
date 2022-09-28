c03445e69d3b00f35cf4876b57683b774f59169e
foxsnow38 <31081148+foxsnow38@users.noreply.github.com>
2022-09-28 13:40:33 +0300

    q3 solve

--- q2.ts
+++ q2.ts
@@ -6,6 +6,7 @@ const reverseFnuc =  (input: number) => {
         const number = Math.floor(input / 2) as number
         if (number > 1) findRecrusive(number)
     }
+    // I CAN MAKE LIKE 9/1 9/2 9/4 BUT THIS SOLVE STYLE IS NOT PRETTY FIT TO RECRUSIVE FUNC IDEA
 
     findRecrusive(input)
    array.reverse().forEach((item:number) => console.log(item));
--- q3.ts
+++ q3.ts
@@ -1,10 +1,18 @@
 
 
 const findRecrusive= (input:string[]) =>{
-
-
-
+const inputObject:any={} // make object
+for (let index = 0; index < input.length; index++) {
+    if(!inputObject[`${input[index]}`])inputObject[`${input[index]}`]=0 //if object keys input[index] is null than change zero
+    inputObject[`${input[index]}`]+=1 // than object keys +1
 }
-findRecrusive(["apple","pie","apple","red","red","red"])
+
+
+return Object.entries(inputObject).sort((a,b):any=> +b[1]- +a[1])[0][0] 
+  // than I convert array to object than I sort.
+  // than I max value take.  
+ // here the a and b problem solve// https://stackoverflow.com/a/68876658/18456713
+}
+console.log(findRecrusive(["apple","pie","apple","red","red","red"]))
 
 export default findRecrusive
