c6c8fc7678b50af53de146d06bfb16ef89ce939c
foxsnow38 <31081148+foxsnow38@users.noreply.github.com>
2022-09-28 13:02:27 +0300

    q2 solved

--- q2.ts
+++ q2.ts
@@ -1,29 +1,13 @@
 
 
-const findADec= (input:string[]) =>{
-
-
-const result  = input.sort((a,b):number=>{
-    let first=0,second=0
-for (let index = 0; index < a.length; index++) {
-      if (  a[index] == `a`) first++  
-  
-}
-for (let index = 0; index < b.length; index++) {
-      if (  b[index] == `a`) second++    
-}
-
-return second-first
-
-})
-
-console.log(result)
+const findRecrusive= (input:number) =>{
+if(input >=1) console.log(input) 
+const number = Math.floor(input/2)  as number
+if(number >1) findRecrusive(number)
 
 
 
 }
-findADec(["aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"])
+findRecrusive(9)
 
-
-
-export default findADec
+export default findRecrusive
