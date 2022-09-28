26f3de0ebb4ef382e8c96b0303fa4e77eced019d
foxsnow38 <31081148+foxsnow38@users.noreply.github.com>
2022-09-28 12:36:38 +0300

    q1 solved

---
+++ q1.ts
@@ -0,0 +1,29 @@
+
+
+const findADec= (input:string[]) =>{
+
+
+const result  = input.sort((a,b):number=>{
+    let first=0,second=0
+for (let index = 0; index < a.length; index++) {
+      if (  a[index] == `a`) first++  
+  
+}
+for (let index = 0; index < b.length; index++) {
+      if (  b[index] == `a`) second++    
+}
+
+return second-first
+
+})
+
+console.log(result)
+
+
+
+}
+findADec(["aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"])
+
+
+
+export default findADec
